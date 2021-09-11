111e8c722c61a30409435aa884dd207bfb5b3ae4
AntwanSherif <antwansherif@gmail.com>
2021-09-11 21:53:06 +0200

    :sparkles: Implement 05. Make the Box component accept a size prop to avoid implementation details (Extra Credit 2)

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,26 +4,31 @@
 import * as React from 'react'
 import '../box-styles.css'
 
-function Box({className = '', style = {}, children}) {
+function Box({size = '', style = {}, children}) {
+  const sizeClassName = size ? `box--${size}` : ''
+
   return (
-    <div className={`box ${className}`} style={{fontStyle: 'italic', ...style}}>
+    <div
+      className={`box ${sizeClassName}`}
+      style={{fontStyle: 'italic', ...style}}
+    >
       {children}
     </div>
   )
 }
 
 const smallBox = (
-  <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+  <Box size="small" style={{backgroundColor: 'lightblue'}}>
     small lightblue box
   </Box>
 )
 const mediumBox = (
-  <Box className="box--medium" style={{backgroundColor: 'pink'}}>
+  <Box size="medium" style={{backgroundColor: 'pink'}}>
     medium pink box
   </Box>
 )
 const largeBox = (
-  <Box className="box--large" style={{backgroundColor: 'orange'}}>
+  <Box size="large" style={{backgroundColor: 'orange'}}>
     large orange box
   </Box>
 )
