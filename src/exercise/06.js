29cc694b926fda6f77b7e48a69b89c0c363d2fd9
AntwanSherif <antwansherif@gmail.com>
2021-09-14 20:44:03 +0200

    :sparkles: Implement 06. Form submission with controled inputs (Extra Credit 3)

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -3,32 +3,16 @@
 
 import * as React from 'react'
 
-function ErrorDisplay({errorMessage}) {
-  if (!errorMessage) return null
-
-  return (
-    <div
-      role="alert"
-      style={{fontSize: 'small', color: 'red', marginBottom: 5}}
-    >
-      {errorMessage}
-    </div>
-  )
-}
-
 function UsernameForm({onSubmitUsername}) {
-  const [error, setError] = React.useState(null)
+  const [userName, setUsername] = React.useState('')
 
   const handleUserNameChange = e => {
     const {value} = e.target
-    const isValidInput = value === value.toLowerCase()
-
-    setError(isValidInput ? null : 'Username must be lower case')
+    setUsername(value.toLowerCase())
   }
 
   const handleSubmit = e => {
     e.preventDefault()
-    const userName = e.target.elements.username.value
     onSubmitUsername(userName)
   }
 
@@ -40,14 +24,11 @@ function UsernameForm({onSubmitUsername}) {
           type="text"
           id="username"
           name="username"
+          value={userName}
           onChange={handleUserNameChange}
         />
-
-        <ErrorDisplay errorMessage={error} />
       </div>
-      <button type="submit" disabled={Boolean(error)}>
-        Submit
-      </button>
+      <button type="submit">Submit</button>
     </form>
   )
 }
