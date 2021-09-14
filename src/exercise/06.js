534cc449d28e117be4a46d929a49e2ab00c96d8d
AntwanSherif <antwansherif@gmail.com>
2021-09-14 20:39:46 +0200

    :sparkles: Implement 06. Form submission with validating the username to be lowercase (Extra Credit 2)

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -3,13 +3,32 @@
 
 import * as React from 'react'
 
+function ErrorDisplay({errorMessage}) {
+  if (!errorMessage) return null
+
+  return (
+    <div
+      role="alert"
+      style={{fontSize: 'small', color: 'red', marginBottom: 5}}
+    >
+      {errorMessage}
+    </div>
+  )
+}
+
 function UsernameForm({onSubmitUsername}) {
-  const userNameRef = React.createRef(null)
+  const [error, setError] = React.useState(null)
+
+  const handleUserNameChange = e => {
+    const {value} = e.target
+    const isValidInput = value === value.toLowerCase()
+
+    setError(isValidInput ? null : 'Username must be lower case')
+  }
 
   const handleSubmit = e => {
     e.preventDefault()
-
-    const userName = userNameRef.current.value
+    const userName = e.target.elements.username.value
     onSubmitUsername(userName)
   }
 
@@ -17,9 +36,18 @@ function UsernameForm({onSubmitUsername}) {
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="username">Username:</label>
-        <input type="text" id="username" name="username" ref={userNameRef} />
+        <input
+          type="text"
+          id="username"
+          name="username"
+          onChange={handleUserNameChange}
+        />
+
+        <ErrorDisplay errorMessage={error} />
       </div>
-      <button type="submit">Submit</button>
+      <button type="submit" disabled={Boolean(error)}>
+        Submit
+      </button>
     </form>
   )
 }
